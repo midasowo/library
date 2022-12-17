dfe18e10a8398a3909c4ae28942e1959ecf7b20a
midasowo <kmiduch@gmail.com>
2022-12-17 18:58:14 +0100

    Added toString methods

--- src/main/java/model/Magazine.java
+++ src/main/java/model/Magazine.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Magazine extends Publication {
     private int month;
     private int day;
@@ -37,9 +39,21 @@ public class Magazine extends Publication {
     }
 
     @Override
-    public void printInfo() {
-        String info = getTitle() + "; " + getPublisher() + "; " + getYear() + "-"
-                + month + "-" + day + "; " + language;
-        System.out.println(info);
+    public String toString() {
+        return super.toString() + ", " + month + ", " + day + ", " + language;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Magazine magazine = (Magazine) o;
+        return month == magazine.month && day == magazine.day && Objects.equals(language, magazine.language);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), month, day, language);
     }
 }
